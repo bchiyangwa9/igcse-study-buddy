5e17627e607d18e975b7b5031f27d0274728ab63
Brian Chiyangwa <bchiyangwa9@gmail.com>
2025-12-30 06:06:10 +0000

    Add Business Studies materials with GitHub URLs
    This file contains updated infographics and videos for Business Studies topics, with infographics now hosted on GitHub and videos remaining on Google Drive.

---
+++ public/static/business-studies-materials.js
@@ -0,0 +1,85 @@
+// Business Studies Additional Materials - UPDATED WITH GITHUB URLS
+// This file contains infographics and videos for Business Studies topics
+// INFOGRAPHICS NOW HOSTED ON GITHUB - VIDEOS REMAIN ON GOOGLE DRIVE
+
+window.BUSINESS_MATERIALS = {
+  'understanding-business-activity': {
+    infographic: {
+      title: 'Business Activity Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-business-activity.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-business-activity.png'
+    },
+    video: {
+      title: 'Understanding Business Activity',
+      embedUrl: 'https://drive.google.com/file/d/1-etZBF9d2wSanUm2iT6r1SUgxmvdZ9Ym/preview'
+    }
+  },
+  'introduction': {
+    infographic: {
+      title: 'Introduction to Business Studies',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-introduction.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-introduction.png'
+    },
+    video: {
+      title: 'Introduction to Business Studies',
+      embedUrl: 'https://drive.google.com/file/d/1wG612Gbi3wdUGfe4r5tP04GZr5I87foJ/preview'
+    }
+  },
+  'exam-technique': {
+    infographic: {
+      title: 'Answering Exam Questions',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-exam-technique.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-exam-technique.png'
+    }
+  },
+  'people-in-business': {
+    infographic: {
+      title: 'People in Business Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-internal-external.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-internal-external.png'
+    },
+    video: {
+      title: 'People in Business',
+      embedUrl: 'https://drive.google.com/file/d/1dvqtfjhF2kDjF1SUWmhSrOT5keo_WOdQ/preview'
+    }
+  },
+  'marketing': {
+    infographic: {
+      title: 'Marketing Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-marketing.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-marketing.png'
+    },
+    video: {
+      title: 'Business Marketing',
+      embedUrl: 'https://drive.google.com/file/d/15tF_jhFzjMofi2Vi820OerItW3Zy57tD/preview'
+    }
+  },
+  'operations-management': {
+    infographic: {
+      title: 'Operations Management Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-operations-management.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-operations-management.png'
+    },
+    video: {
+      title: 'Operations Management',
+      embedUrl: 'https://drive.google.com/file/d/1-etZBF9d2wSanUm2iT6r1SUgxmvdZ9Ym/preview'
+    }
+  },
+  'financial-information': {
+    infographic: {
+      title: 'Financial Information Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-financial-information.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/business-studies/business-studies-financial-information.png'
+    },
+    video: {
+      title: 'Financial Decisions',
+      embedUrl: 'https://drive.google.com/file/d/1XLlLDp9bX5kxFasBy54qyh2B2h66POvx/preview'
+    }
+  },
+  'external-influences': {
+    video: {
+      title: 'External Influences',
+      embedUrl: 'https://drive.google.com/file/d/1nJfIdd0Odz427ogthO6UuAo_fQ7q5EpU/preview'
+    }
+  }
+};
