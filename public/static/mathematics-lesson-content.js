d443418e5d2b28cade79b5ff9753eb6fdef373bc
Brian Chiyangwa <bchiyangwa9@gmail.com>
2025-12-30 06:04:48 +0000

    Add mathematics lesson content with resources
    Added various mathematics lesson materials including infographics and videos hosted on GitHub and Google Drive.

---
+++ public/static/mathematics-lesson-content.js
@@ -0,0 +1,117 @@
+// Mathematics Lesson Content - UPDATED WITH GITHUB URLS
+// This file contains all the lesson content for Mathematics sections
+
+// Additional Learning Materials (Videos and Infographics)
+// INFOGRAPHICS NOW HOSTED ON GITHUB - VIDEOS REMAIN ON GOOGLE DRIVE
+window.MATH_MATERIALS = {
+  'number': {
+    infographic: {
+      title: 'Number Operations Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-number.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-number.png'
+    },
+    video: {
+      title: 'Mastering Number Problems',
+      embedUrl: 'https://drive.google.com/file/d/17eVbjgypI7WOn3S1XHH4vH_-6poxU8ZS/preview'
+    }
+  },
+  'algebra': {
+    infographic: {
+      title: 'Algebra Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-algebra.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-algebra.png'
+    },
+    video: {
+      title: 'Mastering Algebra',
+      embedUrl: 'https://drive.google.com/file/d/12nwAJSGwPdBEq4XbxeygqmS7s5RVd3nJ/preview'
+    }
+  },
+  'functions': {
+    infographic: {
+      title: 'Functions Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-functions.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-functions.png'
+    },
+    video: {
+      title: 'Mastering Maths Functions',
+      embedUrl: 'https://drive.google.com/file/d/12nwAJSGwPdBEq4XbxeygqmS7s5RVd3nJ/preview'
+    }
+  },
+  'coordinate-geometry': {
+    infographic: {
+      title: 'Coordinate Geometry Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-coordinate-geometry.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-coordinate-geometry.png'
+    },
+    video: {
+      title: 'Co-ordinate Geometry',
+      embedUrl: 'https://drive.google.com/file/d/1nJfIdd0Odz427ogthO6UuAo_fQ7q5EpU/preview'
+    }
+  },
+  'geometry': {
+    infographic: {
+      title: 'Geometry Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-geometry.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-geometry.png'
+    },
+    video: {
+      title: 'Cracking Geometry',
+      embedUrl: 'https://drive.google.com/file/d/11mFe3ZC4SUFPy1pOgtm04MkFAIFGsJoa/preview'
+    }
+  },
+  'mensuration': {
+    infographic: {
+      title: 'Mensuration Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-mensuration.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-mensuration.png'
+    },
+    video: {
+      title: 'Mastering Mensuration',
+      embedUrl: 'https://drive.google.com/file/d/1B12kE6xIaWxuhq90fr_nrtjGRpMWJ_I6/preview'
+    }
+  },
+  'trigonometry': {
+    infographic: {
+      title: 'Trigonometry Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-trigonometry.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-trigonometry.png'
+    },
+    video: {
+      title: 'Mastering Trigonometry',
+      embedUrl: 'https://drive.google.com/file/d/1-etZBF9d2wSanUm2iT6r1SUgxmvdZ9Ym/preview'
+    }
+  },
+  'transformations': {
+    infographic: {
+      title: 'Transformations Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-transformations.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-transformations.png'
+    },
+    video: {
+      title: 'Understanding Transformations',
+      embedUrl: 'https://drive.google.com/file/d/1B12kE6xIaWxuhq90fr_nrtjGRpMWJ_I6/preview'
+    }
+  },
+  'probability': {
+    infographic: {
+      title: 'Probability Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-probability.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-probability.png'
+    },
+    video: {
+      title: 'Demystifying Probability',
+      embedUrl: 'https://drive.google.com/file/d/1hfbzIPfHlzIyxUMRi3YH1i_FsXRYJGZz/preview'
+    }
+  },
+  'statistics': {
+    infographic: {
+      title: 'Statistics Visual Guide',
+      url: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-statistics.png',
+      downloadUrl: 'https://raw.githubusercontent.com/bchiyangwa9/igcse-study-buddy/assets/public/assets/images/mathematics/mathematics-statistics.png'
+    },
+    video: {
+      title: 'Mastering Statistics',
+      embedUrl: 'https://drive.google.com/file/d/1yFeP7BSM63q8MpU2erbkRZHeHKHVwW_c/preview'
+    }
+  }
+};
